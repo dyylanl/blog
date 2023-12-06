79887eed89eecb021f20f8a375a06076cc0ea4a6
Dylan <32645436+dyylanl@users.noreply.github.com>
2023-12-06 17:58:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+// Ejemplo de script JavaScript para interactividad (TODO: AMPLIAR)
+// Agrega una clase de animación al hacer clic en un proyecto
+document.querySelectorAll('#projects article').forEach(article => {
+    article.addEventListener('click', () => {
+        article.classList.toggle('active');
+    });
+});
 
